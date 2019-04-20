4f9e664a006b5b444b7d64c22660ce2966dc6823
Galatenea98 <elizabethtenayucra@gmail.com>
2019-04-20 01:51:37 -0500

    espacio

--- Entidades/Espacio.cs
+++ Entidades/Espacio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Entidades
     {
         public int Id { get; set; }
         public bool Disponible { get; set; }
+        public Estacionamiento estacionamiento { get; set; }
 
     }
 }
